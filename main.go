21145aa342e7b5b85a3eb78dc8e597398c99b8b4
z-ohnami <z.ohnami@gmail.com>
2018-03-23 19:04:50 +0900

    message

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-    io.WriteString(w, "変更管理を短くし、構成管理をなくす。内部統制は消去する。")
+    io.WriteString(w, "変更管理を短くし、構成管理をなくす。内部統制は消去する。監査など星の彼方へ")
 }
 
 func main() {
