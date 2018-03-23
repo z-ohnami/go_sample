2d4816a4796d7fd771914bec20efaebce3a778c6
z-ohnami <z.ohnami@gmail.com>
2018-03-23 17:32:05 +0900

    rm

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Exit on any error
-set -e
-
-sudo gcloud docker push us.gcr.io/${PROJECT_ID}/go_sample
-sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl patch deployment docker-hello-google -p '{"spec":{"template":{"spec":{"containers":[{"name":"docker-hello-google","image":"us.gcr.io/ci-sample-198806/go_sample:'"$CIRCLE_SHA1"'"}]}}}}'
